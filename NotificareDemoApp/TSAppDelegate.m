a53b6f19248bd48c53ae784cae42fdc99df28815
jjonthemix <joeloliveira1979@gmail.com>
2013-07-18 16:12:27 +0200

    updated to the latest notificare lib

--- NotificareDemoApp/TSAppDelegate.m
+++ NotificareDemoApp/TSAppDelegate.m
@@ -150,7 +150,6 @@
 }
 
 
-
 -(void)goToSecondTab{
     UITabBarController * tabBarController = (UITabBarController*)self.window.rootViewController;
     [tabBarController setSelectedIndex:1];
@@ -160,14 +159,14 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-   // NSLog(@"applicationWillResignActive");
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    //NSLog(@"applicationDidEnterBackground");
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -180,19 +179,14 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
 //    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-//    NSLog(@"applicationDidBecomeActive");
-//    [[NotificarePushLib shared] launch];
-//    [[NotificarePushLib shared] setDelegate:self];
-//    
-//    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    //NSLog(@"applicationWillTerminate");
-    //[[NotificarePushLib shared] terminate];
+
 }
 
 @end
