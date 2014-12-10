41c397a5726fef261481d146b46ceb16b9e88d74
jjonthemix <joeloliveira1979@gmail.com>
2014-12-10 01:20:29 +0100

    updated lib

---
+++ app/Libs/notificare-push-lib-1.2.2/Notificare/NotificarePushLib/Types/Alert/NotificareAlert.h
@@ -0,0 +1,27 @@
+//
+//  Alert.h
+//  NotificarePushLib
+//
+//  Created by Joel Oliveira on 1/24/13.
+//  Copyright (c) 2013 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NotificareNotification.h"
+#import "NotificareAction.h"
+#import "NotificationType.h"
+#import "NotificationDelegate.h"
+#import "NotificareActions.h"
+#import "NSString+FromBundle.h"
+#import "UIImage+FromBundle.h"
+
+@interface NotificareAlert : NSObject <NotificationType,UIAlertViewDelegate>
+
+@property (nonatomic, assign) id<NotificationDelegate> delegate;
+@property (nonatomic, strong) NotificareNotification * notification;
+@property (nonatomic, strong) NotificareActions * notificareActions;
+
+-(void)openNotification;
+-(void)sendData:(NSArray *)data;
+
+@end
