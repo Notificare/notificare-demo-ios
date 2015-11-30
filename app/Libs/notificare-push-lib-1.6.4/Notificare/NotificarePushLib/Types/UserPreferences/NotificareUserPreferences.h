40294f9b27d4bea1f8674d528d152526ad743048
Joel Oliveira <joel@notifica.re>
2015-11-30 14:41:09 +0100

    updated lib

---
+++ app/Libs/notificare-push-lib-1.6.4/Notificare/NotificarePushLib/Types/UserPreferences/NotificareUserPreferences.h
@@ -0,0 +1,43 @@
+//
+//  WebView.h
+//  NotificarePushLib
+//
+//  Created by Joel Oliveira on 1/24/13.
+//  Copyright (c) 2013 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NotificareNotification.h"
+#import "NotificareAction.h"
+#import "NotificareContent.h"
+#import "NotificareAttachment.h"
+#import "NotificationType.h"
+#import "NotificationDelegate.h"
+#import "NotificareActions.h"
+#import "NSString+FromBundle.h"
+#import "UIImage+FromBundle.h"
+#import "NSDictionary+FromPlist.h"
+#import "NotificareEngine.h"
+#import "NotificareDeviceViewController.h"
+#import "NotificarePassbookViewController.h"
+
+@interface NotificareUserPreferences : NSObject <NotificationType,UIActionSheetDelegate>
+
+@property (nonatomic, assign) id<NotificationDelegate> delegate;
+@property (nonatomic, strong) NotificareNotification * notification;
+@property (nonatomic, strong) NotificareActions * notificareActions;
+@property (strong, nonatomic) NotificareEngine * notificareEngine;
+@property (strong, nonatomic) NotificareDeviceViewController * deviceController;
+@property (strong, nonatomic) NotificarePassbookViewController * passbookController;
+@property (strong, nonatomic) UIViewController * rootViewController;
+@property (strong, nonatomic) UIViewController * viewController;
+@property (strong, nonatomic) UINavigationController * navigationController;
+@property (strong, nonatomic) UIBarButtonItem * closeButton;
+@property (strong, nonatomic) UIBarButtonItem * actionsButton;
+
+
+
+-(void)openNotification;
+-(void)sendData:(NSArray *)data;
+
+@end
